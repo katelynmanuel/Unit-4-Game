6095df958153e8f9405061bf269b6705377833c8
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-06-12 21:15:17 -0600

    Adding images, some tweaks to HTML content, and more script to support game functions.

--- assets/game.js
+++ assets/game.js
@@ -2,20 +2,44 @@ $(document).ready(function() {
 
 //Define global variables. 
 var computerGeneratedNumber = 0;
-var inPlayUserValue = 0;
+var gemstoneValue = 0;
+var counter = 0;
 var userWins = 0;
 var userLosses = 0;
 
 //Function for computer to select random number between 19-120
 computerGeneratedNumber = Math.floor(Math.random() * 102) + 19;
 console.log(computerGeneratedNumber);
+$(".matchTargetNumber").append("Target Number to Match: " + computerGeneratedNumber);
 
-//Function or functions for selecting random number for crystals 1-12.
-//Assign random number to crystals
+//Function(s) for selecting random number for crystals 1-12 when clicked on.
 //Make crystals on click events
-//Once user clicks on a crystal user number increments by that value
-//if else statement to check if user vaalue equals computer value or is greater than
+$(".crystalImages").on("click", function() {
+
+    //Assign random number to crystals
+    gemstoneValue = Math.floor(Math.random() * 12) + 1;
+    console.log(gemstoneValue);
+
+    //Add user's total value for individual clicks to displayUserNumber 
+    $("#gemstone1", "#gemstone2", "#gemstone3", "#gemstone4").append(gemstoneValue);
+    counter += gemstoneValue;
+
+    //Display to displayUserNumber div.
+    $(".displayUserNumber").append(counter);
+
+    if (counter === computerGeneratedNumber) {
+        $(".resultsPanel").append("You Won!");
+    } else if (counter > computerGeneratedNumber) {
+        $(".resultsPanel").append("You Lost!");
+    };
+})
+
+
+
+//
+//if else statement to check if user value equals computer value or is greater than
 //if player value = computer value user wins go up by one and game resets. 
 //if player value > computer value loss goes up and game resets. 
 
+});
 
