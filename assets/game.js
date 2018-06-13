afe44e6eeaae70d1cc63a89633e5d63c4364fae3
Katelyn Manuel <kmanuel@osc-laptop-44.osmosissc.com>
2018-06-12 18:53:40 -0600

    Adding initial HTML layout and first steps to creating variables and random number generator for computer.

---
+++ assets/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+//Define global variables. 
+var computerGeneratedNumber = 0;
+var inPlayUserValue = 0;
+var userWins = 0;
+var userLosses = 0;
+
+//Function for computer to select random number between 19-120
+computerGeneratedNumber = Math.floor(Math.random() * 102) + 19;
+console.log(computerGeneratedNumber);
+
+//Function or functions for selecting random number for crystals 1-12.
+//Assign random number to crystals
+//Make crystals on click events
+//Once user clicks on a crystal user number increments by that value
+//if else statement to check if user vaalue equals computer value or is greater than
+//if player value = computer value user wins go up by one and game resets. 
+//if player value > computer value loss goes up and game resets. 
+
+
